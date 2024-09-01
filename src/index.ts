8b0163e5fb57b55d44d954ea43a517be3676d9c0
C. Daniel <daniel@thedaniweb.eu.org>
2024-09-01 17:53:46 -0400

    A  .gitignore A  README.md A  package.json A  pnpm-lock.yaml A  src/index.ts A  tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { serve } from "@hono/node-server";
+import playwright from "playwright-core";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+const token = "QlsEnjEptEj6sG9103b502c30acc4d1574803f8441";
+const pwdEndpoint = `wss://production-sfo.browserless.io/firefox/playwright?token=${token}`;
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/screen", async (c) => {
+  try {
+    const browser = await playwright.firefox.connect(pwdEndpoint);
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://api.chatwars.me/webview/map");
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const screenBuffer = await page.locator(".map-grid-wrapper").screenshot();
+    await browser.close();
+    return c.body(screenBuffer, {
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "no-cache",
+      },
+    });
+  } catch (error) {
+    console.error("Error taking the screenshoot ", error);
+    return c.body("Error taking the screenshot ", { status: 500 });
+  }
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
